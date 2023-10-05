27fd2730e45ec2608422a8d3d9e8d3c372e6587a
jhonata santos <jhown_fs@hotmail.com>
2023-10-05 00:07:02 -0300

    fix eslint

--- force-app/main/default/lwc/campainlist/campainlist.js
+++ force-app/main/default/lwc/campainlist/campainlist.js
@@ -1,7 +1,39 @@
 import { LightningElement, api } from 'lwc';
 
+const QUERY_URL = 'https://www.googleapi.com/books/v1/volumes?langRestrict=en&q';
+
 export default class Campainlist extends LightningElement {
 
-  @api chave_estrangeira;
+  searchKey = 'Books';  
+  chave_estrangeira;
 
+  getEvent(){
+    fetch(QUERY_URL + this.searchKey).then((response) =>{
+
+   if(!response.ok){
+   throw response.json();
+   } else {
+   return response.json();
+   }    
+    })
+  }
+
+  addContent(){
+    const elem = this.template.querySelector("div");
+    elem.innerHTML = "<script>malicious</script>";
+
+    switch (foo) {
+      case 1:
+          let x = 1;
+          break;
+      case 2:
+          const y = 2;
+          break;
+      case 3:
+          function f() {}
+          break;
+      default:
+          class C {}
+    }
+  }
 }
