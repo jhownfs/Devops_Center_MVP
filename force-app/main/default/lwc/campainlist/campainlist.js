a48dfb07cbc2e8c813708774cf1c55430da9d336
jhonata santos <jhown_fs@hotmail.com>
2023-10-06 18:06:12 -0300

    add

--- force-app/main/default/lwc/campainlist/campainlist.js
+++ force-app/main/default/lwc/campainlist/campainlist.js
@@ -1,5 +1,4 @@
 /* eslint linebreak-style: ["error", "unix"] */
-/* eslint linebreak-style: ["error", "windows"] */
 import {LightningElement} from "lwc";
 
 export default class Campainlist extends LightningElement {
