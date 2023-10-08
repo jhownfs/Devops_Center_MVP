e6ee4b55a84a649fd67dbdf93e44df349ea9dba7
jhonata santos <jhown_fs@hotmail.com>
2023-10-08 12:12:25 -0300

    alguns ajustes

--- force-app/main/default/lwc/campainlist/campainlist.js
+++ force-app/main/default/lwc/campainlist/campainlist.js
@@ -1,8 +1,26 @@
-/* eslint linebreak-style: ["error", "unix"] */
-import {LightningElement} from "lwc";
+/* eslint linebreak-style: ["error", "windows"] */
+import {LightningElement, track, api} from "lwc";
+import getCupons from "@salesforce/apex/AttachCampaings.getCupons";
 
 export default class Campainlist extends LightningElement {
 
-    nameL = "teste";
+    @track 
+    lstCupons = [];
+    @api 
+    validado = false;
 
+    // Utlizando promise para chamar o metodo imperativo
+    @api
+    handleLoad() {
+        getCupons().then((result) => {
+
+            this.lstCupons = result;
+            const inputs = this.template.querySelectorAll('.form-control');
+
+            inputs.textContent = 'mensagem';
+        })
+        .catch((error) => {
+            console.log("error => ", error);
+        });
+    }
 }
