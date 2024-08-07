c5f27ea36f7a3b415d4af7b768cc65a973586601
jhonata santos <jhown_fs@hotmail.com>
2024-08-06 21:43:28 -0300

    fix: permission

--- pipeline/helpers/authetication.sh
+++ pipeline/helpers/authetication.sh
@@ -2,6 +2,6 @@
 
 set -e 
 
-echo "$JWT_SECRET"  > secret.key
+echo "$SECRET"  > secret.key
 echo "#### Step to Authenticate in Organization #####"
- ##sf org login jwt --username $USERNAME --jwt-key-file ./secret.key --client-id $KEY --alias SFDX --instance-url $URL
+ sf org login jwt --username $USERNAME --jwt-key-file ./secret.key --client-id $KEY --alias SFDX --instance-url $URL
